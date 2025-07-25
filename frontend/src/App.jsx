d481931b26401d15541fe8f50b53137388a148a1
Aashutosh Uikey <aashureal01@gmail.com>
2025-07-25 13:15:41 +0530

    UI update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <main>
+    <main className="w-screen  min-h-screen bg-black text-white px-5 py-24">
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="w-[94%] bg-zinc-0">
+      {/* Video Container */}
+      <div className="video__container">
+        <div className="video bg-red-500 w-full aspect-video rounded-2xl"></div>
+        <button className="text-xl uppercase w-full bg-zinc-800 py-5 rounded-2xl my-7">
+          Detact Mood
+        </button>
+      </div>
 
-export default Home
+      {/* Music Container */}
+      <div className="music__container">
+        <div className="header py-5 border-y border-zinc-800">
+          <h2 className="heading text-3xl font-semibold">Songs</h2>
+        </div>
+
+        {/* Songs */}
+        <div className="songs"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
